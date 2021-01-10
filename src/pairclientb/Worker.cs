70b60b4aa006203875480cf8e3cc77b8a8326a6e
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2021-01-10 19:12:05 +0100

    Add paircalculator b and c
    Add paircalculator b and c

---
+++ src/pairclientb/Worker.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using microservicesapp;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace pairclientb
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly PairCalculator.PairCalculatorClient _pairClient;
+
+        public Worker(ILogger<Worker> logger, PairCalculator.PairCalculatorClient pairClient)
+        {
+            _logger = logger;
+            _pairClient = pairClient;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //Giving time for all other services/dependencies to be warmed up (ex: RabbitMQ takes time to boot up)
+            await Task.Delay(TimeSpan.FromSeconds(33), stoppingToken);
+            _logger.LogInformation("Starting to send Pair number requests.......");
+
+            long input = 100002;
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var response = await _pairClient.IsItPairAsync(new PairRequest { Number = input });
+                    _logger.LogInformation($"Is {input} a Pair number? Service tells us: {response.IsPair}\r");
+                }
+                catch (Exception ex)
+                {
+                    if (stoppingToken.IsCancellationRequested) return;
+                    _logger.LogError(-1, ex, "Error occurred while calling IsItPairAsync() but will continue..");
+                    await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken); //just adding some delay in case of error..
+                }
+
+                input = input + 2;
+
+                if (stoppingToken.IsCancellationRequested) break;
+
+                await Task.Delay(TimeSpan.FromMilliseconds(10), stoppingToken);
+            }
+        }
+    }
+}
---
+++ src/pairclientc/Program.cs
@@ -0,0 +1,33 @@
+using microservicesapp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace pairclientc
+{
+    public class Program
+    {
+        private static readonly Uri MANUAL_DEBUGTIME_URI = new Uri("https://localhost:5051"); //From launchSettings of the paircalculator
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var configurationFromHostBuilderContext = hostContext.Configuration;
+
+                    services.AddHostedService<Worker>();
+
+                    services.AddGrpcClient<PairCalculator.PairCalculatorClient>(o =>
+                    {
+                        //paircalculator will be inject to configuration via Tye
+                        o.Address = configurationFromHostBuilderContext.GetServiceUri("paircalculator") ?? MANUAL_DEBUGTIME_URI;
+                    });
+                });
+    }
+}
